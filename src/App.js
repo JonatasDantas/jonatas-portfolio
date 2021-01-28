11d6dff3986f29e46d041e0d859985a6ec10a310
JonatasDantas <jonatas.almeida14@hotmail.com>
2021-01-28 11:22:28 -0300

    Project's initial structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './sections/home/Home';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { useScrollTrigger } from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+import './App.scss';
+import { useState } from 'react';
 
 function App() {
+  const fixedNav = useScrollTrigger({ threshold: window.innerHeight });
+  const sections = ["Home", "Sobre mim", "Habilidades", "Projetos", "Contato"]
+  const [drawerOpen, setDrawer] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Home />
+
+      <AppBar className="navbar" position="sticky">
+        <Toolbar className="toolbar" variant="dense">
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={() => setDrawer(true)}
+            edge="start"
+          >
+            <MenuIcon />
+          </IconButton>
+
+          {sections.map((text, index) => (
+            <Typography variant="h6" className="nav-item" key={index}>{text}</Typography>
+          ))}
+        </Toolbar>
+      </AppBar>
+
+      <SwipeableDrawer
+        anchor="left"
+        open={drawerOpen}
+        onClick={() => setDrawer(false)}
+        onKeyDown={() => setDrawer(false)}
+        style={{width: 240}}
+      >
+        <List>
+          {sections.map((text, index) => (
+            <ListItem button key={index}>
+              {/* <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon> */}
+
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+
+      </SwipeableDrawer>
+      <div style={{ height: 1000 }}>
+
+      </div>
     </div>
   );
 }
---
+++ src/sections/home/Home.js
@@ -0,0 +1,99 @@
+import Particles from 'react-particles-js';
+import { makeStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+import userImg from '../../assets/img/avatar.jpg';
+import Button from '@material-ui/core/Button';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+
+import './Home.scss';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        '& > *': {
+            margin: theme.spacing(1),
+        },
+    },
+    large: {
+        width: theme.spacing(26),
+        height: theme.spacing(26),
+        display: "inline-flex",
+    },
+    button: {
+        zIndex: 3
+    }
+}));
+
+const Home = () => {
+    const classes = useStyles();
+
+    return (
+        <div className="home">
+            <div className="info">
+                <h1>Jonatas de Almeida Dantas</h1>
+
+                <div className="welcoming-text">
+                    Olá! Meu nome é Jonatas, sou desenvolvedor Full-Stack. Atuo em projetos de alta complexidade
+                    que atendem milhares de clientes em todo o Brasil.
+
+                    <br /><br /><br />
+                    {/* 
+                    Tenho um conjunto de habilidades muito diverso, variando de tecnologias para desenvolvimento web, 
+                    mobile multiplataforma, design, infraestrutura e habilidades intrapessoais.
+                    <br/><br/> */}
+
+                </div>
+
+                <Button
+                    variant="contained"
+                    color="default"
+                    size="large"
+                    className={classes.button}
+                    endIcon={<ArrowForwardIcon />}
+                >
+                    Conheça meu trabalho
+                </Button>
+
+                <br /><br />
+
+                <div className="image">
+                    <Avatar alt="Jonatas de Almeida" src={userImg} className={classes.large} />
+                </div>
+            </div>
+
+            <Particles
+                params={{
+                    particles: {
+                        number: {
+                            value: 200,
+                            density: {
+                                enable: true,
+                                value_area: 1000,
+                            }
+                        },
+                    },
+                    interactivity: {
+                        events: {
+                            onhover: {
+                                enable: true,
+                                mode: "repulse"
+                            }
+                        }
+                    }
+                }}
+            />
+            
+            {/* <nav className="navigation">
+                <div className="menu">
+                    <div className="menuItem">Home</div>
+                    <div className="menuItem">Sobre Mim</div>
+                    <div className="menuItem">Habilidades</div>
+                    <div className="menuItem">Projetos</div>
+                    <div className="menuItem">Contato</div>
+                </div>
+            </nav> */}
+        </div>
+    )
+}
+
+export default Home;
